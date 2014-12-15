93a99e77930d56de6d28e11f04c27193e67bdd0d
Fabio Tavares <fabreder@gmail.com>
2014-12-15 15:35:52 -0200

    Atualizando gitignore

--- bin/cli-config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-//php ../vendor/doctrine/orm/bin/doctrine.php
-
-use Doctrine\ORM\Tools\Console\ConsoleRunner;
-
-require_once '../bootstrap.php';
-return ConsoleRunner::createHelperSet($em);
